e0b96bc49051e1ee87836a0d23c89b2d8837ad5c
Eugene Skrobov <skrobov.ev@gmail.com>
2024-06-23 21:43:14 +0700

    Implement quickSort

--- src/quickSort/quickSort.ts
+++ src/quickSort/quickSort.ts
@@ -1,12 +1,33 @@
-const compareFunctions = {
-    asc: (a: number, b: number): number => a - b,
-    desc: (a: number, b: number): number => b - a
-} as const;
-
 export const quickSort = (items: number[], direction: 'asc' | 'desc' = 'asc'): number[] => {
-    const clonedItems = [...items];
+    if (items.length <= 1) {
+        return items;
+    }
 
-    const compareItems = compareFunctions[direction];
+    const referenceItem = items[0];
 
-    return clonedItems.sort(compareItems);
+    const lowerItems: number[] = [];
+    const middleItems: number[] = [];
+    const greaterItems: number[] = [];
+
+    items.forEach((item) => {
+        if (item > referenceItem) {
+            greaterItems.push(item);
+            return;
+        }
+
+        if (item < referenceItem) {
+            lowerItems.push(item);
+            return;
+        }
+
+        middleItems.push(item);
+    });
+
+    const sortedAscendingItems = quickSort(lowerItems).concat(...middleItems, ...quickSort(greaterItems));
+
+    if (direction === 'desc') {
+        return sortedAscendingItems.reverse();
+    }
+
+    return sortedAscendingItems;
 }
