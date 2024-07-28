98fd72293f51b44616dce0c5e9e5be60bb398f5c
Eugene Skrobov <skrobov.ev@gmail.com>
2024-07-28 19:52:53 +0700

    Add heap tests

--- src/heap/heap.test.ts
+++ src/heap/heap.test.ts
@@ -108,7 +108,7 @@ describe('Heap', () => {
           expect(size).toBe(6);
         });
 
-        test.skip('getRootNode after extraction should return 5', () => {
+        test('getRootNode after extraction should return 5', () => {
           const rootNode = heap.getRootNode();
           expect(rootNode).toBe(5);
         });
@@ -205,7 +205,7 @@ describe('Heap', () => {
           expect(size).toBe(5);
         });
 
-        test.skip('getRootNode after extraction should return 4', () => {
+        test('getRootNode after extraction should return 4', () => {
           const rootNode = heap.getRootNode();
           expect(rootNode).toBe(4);
         });
@@ -302,7 +302,7 @@ describe('Heap', () => {
           expect(size).toBe(4);
         });
 
-        test.skip('getRootNode after extraction should return 3', () => {
+        test('getRootNode after extraction should return 3', () => {
           const rootNode = heap.getRootNode();
           expect(rootNode).toBe(3);
         });
@@ -478,7 +478,7 @@ describe('Heap', () => {
           expect(size).toBe(6);
         });
 
-        test.skip('getRootNode after extraction should return 5', () => {
+        test('getRootNode after extraction should return 5', () => {
           const rootNode = heap.getRootNode();
           expect(rootNode).toBe(5);
         });
@@ -542,6 +542,200 @@ describe('Heap', () => {
           test('extractedRoot should be 7', () => {
             const extractedRoot = heap.extractRootNode();
             expect(extractedRoot).toBe(7);
+          });
+        });
+      });
+    });
+
+    describe('Nodes 0 1 2 3 4 5', () => {
+      const nodes = [0, 1, 2, 3, 4, 5];
+
+      test('getSize should return 6', () => {
+        const heap = new Heap(nodes, compareNumbersAscending);
+        const size = heap.getSize();
+        expect(size).toBe(6);
+      });
+
+      test('getRootNode should return 5', () => {
+        const heap = new Heap(nodes, compareNumbersAscending);
+        const rootNode = heap.getRootNode();
+        expect(rootNode).toBe(5);
+      });
+
+      describe('extractRootNode', () => {
+        const heap = new Heap(nodes, compareNumbersAscending);
+        const extractedRoot = heap.extractRootNode();
+
+        test('extractedRoot should be 5', () => {
+          expect(extractedRoot).toBe(5);
+        });
+
+        test('getSize after extraction should return 5', () => {
+          const size = heap.getSize();
+          expect(size).toBe(5);
+        });
+
+        test('getRootNode after extraction should return 4', () => {
+          const rootNode = heap.getRootNode();
+          expect(rootNode).toBe(4);
+        });
+      });
+
+      describe('addNode', () => {
+        describe('add min value -1', () => {
+          const heap = new Heap(nodes, compareNumbersAscending);
+          heap.addNode(-1);
+
+          test('getSize after adding should return 7', () => {
+            const size = heap.getSize();
+            expect(size).toBe(7);
+          });
+
+          test('getRootNode after adding should return 5', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(5);
+          });
+
+          test('extractedRoot should be 5', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(5);
+          });
+        });
+
+        describe('add average value 3', () => {
+          const heap = new Heap(nodes, compareNumbersAscending);
+          heap.addNode(3);
+
+          test('getSize after adding should return 7', () => {
+            const size = heap.getSize();
+            expect(size).toBe(7);
+          });
+
+          test('getRootNode after adding should return 5', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(5);
+          });
+
+          test('extractedRoot should be 5', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(5);
+          });
+        });
+
+        describe('add max value 7', () => {
+          const heap = new Heap(nodes, compareNumbersAscending);
+          heap.addNode(7);
+
+          test('getSize after adding should return 7', () => {
+            const size = heap.getSize();
+            expect(size).toBe(7);
+          });
+
+          test('getRootNode after adding should return 7', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(7);
+          });
+
+          test('extractedRoot should be 7', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(7);
+          });
+        });
+      });
+    });
+
+    describe('Nodes 0 1 2 3 4', () => {
+      const nodes = [0, 1, 2, 3, 4];
+
+      test('getSize should return 5', () => {
+        const heap = new Heap(nodes, compareNumbersAscending);
+        const size = heap.getSize();
+        expect(size).toBe(5);
+      });
+
+      test('getRootNode should return 4', () => {
+        const heap = new Heap(nodes, compareNumbersAscending);
+        const rootNode = heap.getRootNode();
+        expect(rootNode).toBe(4);
+      });
+
+      describe('extractRootNode', () => {
+        const heap = new Heap(nodes, compareNumbersAscending);
+        const extractedRoot = heap.extractRootNode();
+
+        test('extractedRoot should be 4', () => {
+          expect(extractedRoot).toBe(4);
+        });
+
+        test('getSize after extraction should return 4', () => {
+          const size = heap.getSize();
+          expect(size).toBe(4);
+        });
+
+        test('getRootNode after extraction should return 3', () => {
+          const rootNode = heap.getRootNode();
+          expect(rootNode).toBe(3);
+        });
+      });
+
+      describe('addNode', () => {
+        describe('add min value -1', () => {
+          const heap = new Heap(nodes, compareNumbersAscending);
+          heap.addNode(-1);
+
+          test('getSize after adding should return 6', () => {
+            const size = heap.getSize();
+            expect(size).toBe(6);
+          });
+
+          test('getRootNode after adding should return 4', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(4);
+          });
+
+          test('extractedRoot should be 4', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(4);
+          });
+        });
+
+        describe('add average value 2', () => {
+          const heap = new Heap(nodes, compareNumbersAscending);
+          heap.addNode(2);
+
+          test('getSize after adding should return 6', () => {
+            const size = heap.getSize();
+            expect(size).toBe(6);
+          });
+
+          test('getRootNode after adding should return 4', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(4);
+          });
+
+          test('extractedRoot should be 4', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(4);
+          });
+        });
+
+        describe('add max value 5', () => {
+          const heap = new Heap(nodes, compareNumbersAscending);
+          heap.addNode(5);
+
+          test('getSize after adding should return 6', () => {
+            const size = heap.getSize();
+            expect(size).toBe(6);
+          });
+
+          test('getRootNode after adding should return 5', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(5);
+          });
+
+          test('extractedRoot should be 5', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(5);
           });
         });
       });
@@ -654,7 +848,7 @@ describe('Heap', () => {
           expect(size).toBe(6);
         });
 
-        test.skip('getRootNode after extraction should return 1', () => {
+        test('getRootNode after extraction should return 1', () => {
           const rootNode = heap.getRootNode();
           expect(rootNode).toBe(1);
         });
@@ -711,6 +905,200 @@ describe('Heap', () => {
           });
 
           test('getSize after adding should return 0', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(0);
+          });
+
+          test('extractedRoot should be 0', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(0);
+          });
+        });
+      });
+    });
+
+    describe('Nodes 0 1 2 3 4 5', () => {
+      const nodes = [0, 1, 2, 3, 4, 5];
+
+      test('getSize should return 6', () => {
+        const heap = new Heap(nodes, compareNumbersDescending);
+        const size = heap.getSize();
+        expect(size).toBe(6);
+      });
+
+      test('getRootNode should return 0', () => {
+        const heap = new Heap(nodes, compareNumbersDescending);
+        const rootNode = heap.getRootNode();
+        expect(rootNode).toBe(0);
+      });
+
+      describe('extractRootNode', () => {
+        const heap = new Heap(nodes, compareNumbersDescending);
+        const extractedRoot = heap.extractRootNode();
+
+        test('extractedRoot should be 0', () => {
+          expect(extractedRoot).toBe(0);
+        });
+
+        test('getSize after extraction should return 5', () => {
+          const size = heap.getSize();
+          expect(size).toBe(5);
+        });
+
+        test('getRootNode after extraction should return 1', () => {
+          const rootNode = heap.getRootNode();
+          expect(rootNode).toBe(1);
+        });
+      });
+
+      describe('addNode', () => {
+        describe('add min value -1', () => {
+          const heap = new Heap(nodes, compareNumbersDescending);
+          heap.addNode(-1);
+
+          test('getSize after adding should return 7', () => {
+            const size = heap.getSize();
+            expect(size).toBe(7);
+          });
+
+          test('getRootNode after adding should return -1', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(-1);
+          });
+
+          test('extractedRoot should be -1', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(-1);
+          });
+        });
+
+        describe('add average value 3', () => {
+          const heap = new Heap(nodes, compareNumbersDescending);
+          heap.addNode(3);
+
+          test('getSize after adding should return 7', () => {
+            const size = heap.getSize();
+            expect(size).toBe(7);
+          });
+
+          test('getSize after adding should return 0', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(0);
+          });
+
+          test('extractedRoot should be 0', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(0);
+          });
+        });
+
+        describe('add max value 7', () => {
+          const heap = new Heap(nodes, compareNumbersDescending);
+          heap.addNode(7);
+
+          test('getSize after adding should return 8', () => {
+            const size = heap.getSize();
+            expect(size).toBe(7);
+          });
+
+          test('getSize after adding should return 0', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(0);
+          });
+
+          test('extractedRoot should be 0', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(0);
+          });
+        });
+      });
+    });
+
+    describe('Nodes 0 1 2 3 4', () => {
+      const nodes = [0, 1, 2, 3, 4];
+
+      test('getSize should return 5', () => {
+        const heap = new Heap(nodes, compareNumbersDescending);
+        const size = heap.getSize();
+        expect(size).toBe(5);
+      });
+
+      test('getRootNode should return 0', () => {
+        const heap = new Heap(nodes, compareNumbersDescending);
+        const rootNode = heap.getRootNode();
+        expect(rootNode).toBe(0);
+      });
+
+      describe('extractRootNode', () => {
+        const heap = new Heap(nodes, compareNumbersDescending);
+        const extractedRoot = heap.extractRootNode();
+
+        test('extractedRoot should be 0', () => {
+          expect(extractedRoot).toBe(0);
+        });
+
+        test('getSize after extraction should return 4', () => {
+          const size = heap.getSize();
+          expect(size).toBe(4);
+        });
+
+        test('getRootNode after extraction should return 1', () => {
+          const rootNode = heap.getRootNode();
+          expect(rootNode).toBe(1);
+        });
+      });
+
+      describe('addNode', () => {
+        describe('add min value -1', () => {
+          const heap = new Heap(nodes, compareNumbersDescending);
+          heap.addNode(-1);
+
+          test('getSize after adding should return 6', () => {
+            const size = heap.getSize();
+            expect(size).toBe(6);
+          });
+
+          test('getRootNode after adding should return -1', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(-1);
+          });
+
+          test('extractedRoot should be -1', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(-1);
+          });
+        });
+
+        describe('add average value 2', () => {
+          const heap = new Heap(nodes, compareNumbersDescending);
+          heap.addNode(2);
+
+          test('getSize after adding should return 6', () => {
+            const size = heap.getSize();
+            expect(size).toBe(6);
+          });
+
+          test('getRootNode after adding should return 0', () => {
+            const rootNode = heap.getRootNode();
+            expect(rootNode).toBe(0);
+          });
+
+          test('extractedRoot should be 0', () => {
+            const extractedRoot = heap.extractRootNode();
+            expect(extractedRoot).toBe(0);
+          });
+        });
+
+        describe('add max value 5', () => {
+          const heap = new Heap(nodes, compareNumbersDescending);
+          heap.addNode(5);
+
+          test('getSize after adding should return 6', () => {
+            const size = heap.getSize();
+            expect(size).toBe(6);
+          });
+
+          test('getRootNode after adding should return 0', () => {
             const rootNode = heap.getRootNode();
             expect(rootNode).toBe(0);
           });
