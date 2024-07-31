e1d938416bf07dd96932f8238fa6d7b69eba0768
Eugene Skrobov <skrobov.ev@gmail.com>
2024-07-31 19:26:09 +0700

    Improve graph tests

--- src/graph/UndirectedGraph.test.ts
+++ src/graph/UndirectedGraph.test.ts
@@ -79,7 +79,7 @@ describe('UndirectedGraph', () => {
     });
   });
 
-  describe.skip('Graph with single edge 1-2', () => {
+  describe('Graph with single edge 1-2', () => {
     describe('Graph initialized with [1, 2]', () => {
       const from = 1;
       const to = 2;
@@ -291,6 +291,127 @@ describe('UndirectedGraph', () => {
           );
           expect(hasNonExistentEdgeResult).toBe(false);
         });
+      });
+    });
+
+    describe('Graph with A-B and B-C edges', () => {
+      const aNode = 'A';
+      const bNode = 'B';
+      const cNode = 'C';
+      const edges: [string, string][] = [
+        [aNode, bNode],
+        [bNode, cNode],
+      ];
+      const graph = new UndirectedGraph(edges);
+
+      test(`Node with ${aNode} key should be in the graph`, () => {
+        const hasNodeResult = graph.hasNode(aNode);
+        expect(hasNodeResult).toBe(true);
+      });
+
+      test(`Node with ${bNode} key should be in the graph`, () => {
+        const hasNodeResult = graph.hasNode(bNode);
+        expect(hasNodeResult).toBe(true);
+      });
+
+      test(`Node with ${cNode} key should be in the graph`, () => {
+        const hasNodeResult = graph.hasNode(cNode);
+        expect(hasNodeResult).toBe(true);
+      });
+
+      test(`getNodes should return array with 3 keys`, () => {
+        const nodes = graph.getNodes();
+        expect(nodes.length).toBe(3);
+      });
+
+      test(`getNodes should return array containing ${aNode} key`, () => {
+        const nodes = graph.getNodes();
+        expect(nodes).toContain(aNode);
+      });
+
+      test(`getNodes should return array containing ${bNode} key`, () => {
+        const nodes = graph.getNodes();
+        expect(nodes).toContain(bNode);
+      });
+
+      test(`getNodes should return array containing ${cNode} key`, () => {
+        const nodes = graph.getNodes();
+        expect(nodes).toContain(cNode);
+      });
+
+      test(`Edge from ${aNode} to ${bNode} should be in the graph`, () => {
+        const hasEdgeResult = graph.hasEdge(aNode, bNode);
+        expect(hasEdgeResult).toBe(true);
+      });
+
+      test(`Edge from ${bNode} to ${aNode} should be in the graph`, () => {
+        const hasEdgeResult = graph.hasEdge(bNode, aNode);
+        expect(hasEdgeResult).toBe(true);
+      });
+
+      test(`Edge from ${bNode} to ${cNode} should be in the graph`, () => {
+        const hasEdgeResult = graph.hasEdge(bNode, cNode);
+        expect(hasEdgeResult).toBe(true);
+      });
+
+      test(`Edge from ${cNode} to ${bNode} should be in the graph`, () => {
+        const hasEdgeResult = graph.hasEdge(cNode, bNode);
+        expect(hasEdgeResult).toBe(true);
+      });
+
+      test(`Edge from ${aNode} to ${cNode} should not be in the graph`, () => {
+        const hasEdgeResult = graph.hasEdge(aNode, cNode);
+        expect(hasEdgeResult).toBe(false);
+      });
+
+      test(`Edge from ${cNode} to ${aNode} should not be in the graph`, () => {
+        const hasEdgeResult = graph.hasEdge(cNode, aNode);
+        expect(hasEdgeResult).toBe(false);
+      });
+
+      test(`Node with ${aNode} key should have single direction`, () => {
+        const directions = graph.getNodeDirections(aNode);
+        expect(directions.length).toBe(1);
+      });
+
+      test(`Node with ${aNode} key should have direction to ${bNode}`, () => {
+        const directions = graph.getNodeDirections(aNode);
+        expect(directions).toContain(bNode);
+      });
+
+      test(`Node with ${aNode} key should not have direction to ${cNode}`, () => {
+        const directions = graph.getNodeDirections(aNode);
+        expect(directions).not.toContain(cNode);
+      });
+
+      test(`Node with ${bNode} key should have two directions`, () => {
+        const directions = graph.getNodeDirections(bNode);
+        expect(directions.length).toBe(2);
+      });
+
+      test(`Node with ${bNode} key should have direction to ${aNode}`, () => {
+        const directions = graph.getNodeDirections(bNode);
+        expect(directions).toContain(aNode);
+      });
+
+      test(`Node with ${bNode} key should have direction to ${cNode}`, () => {
+        const directions = graph.getNodeDirections(bNode);
+        expect(directions).toContain(cNode);
+      });
+
+      test(`Node with ${cNode} key should have single direction`, () => {
+        const directions = graph.getNodeDirections(cNode);
+        expect(directions.length).toBe(1);
+      });
+
+      test(`Node with ${cNode} key should have direction to ${bNode}`, () => {
+        const directions = graph.getNodeDirections(cNode);
+        expect(directions).toContain(bNode);
+      });
+
+      test(`Node with ${cNode} key should not have direction to ${aNode}`, () => {
+        const directions = graph.getNodeDirections(cNode);
+        expect(directions).not.toContain(aNode);
       });
     });
   });
